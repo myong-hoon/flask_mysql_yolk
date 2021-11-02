9ae8724e8d9632c547c9da40455e1e2dd6703ce0
myong-hoon <92171034+myong-hoon@users.noreply.github.com>
2021-11-02 23:46:54 +0900

    login 추가

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from pymongo import MongoClient
 import pymysql
 import jwt
@@ -29,9 +30,13 @@ def home():
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
         # Client info 보내주기
-        user_info = db.users.find_one({"username": payload["id"]})
-        print(user_info)
-        return render_template('index.html', user_info=user_info)
+        # user_info = db.users.find_one({"username": payload["id"]})
+        
+        sql = "SELECT * FROM users where username = '%s';"
+        cursor.execute(sql%(payload["id"]))
+        result = cursor.fetchone()
+
+        return render_template('index.html', user_info=result)
 
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -53,8 +58,13 @@ def user(username):
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
 
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
+        # user_info = db.users.find_one({"username": username}, {"_id": False})
+        
+        sql = "SELECT * FROM users where username = '%s';"
+        cursor.execute(sql%(username))
+        result = cursor.fetchone()
+
+        return render_template('user.html', user_info=result, status=status)
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
@@ -66,9 +76,14 @@ def sign_in():
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+    # result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
-    if result is not None:
+    sql = "SELECT username, password FROM users"
+    cursor.execute(sql)
+    loginResult = cursor.fetchall()
+    loginResultCount = loginResult.count({'username': username_receive, 'password': pw_hash})
+
+    if loginResultCount == 1:
         payload = {
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
@@ -98,12 +113,12 @@ def sign_up():
     }
 
 
-    sql = "insert into users(username, password, profile_name, profile_pic_real) values(%s, %s, %s, 'profile_pics/profile_placeholder.png')"
+    sql = "insert into users(username, password, profile_name, profile_pic_real,profile_pic,profile_info) values(%s, %s, %s, 'profile_pics/profile_placeholder.png',"","")"
 
     cursor.execute(sql,(username_receive,password_hash,username_receive))
     connect.commit()
 
-    db.users.insert_one(doc)
+    # db.users.insert_one(doc)
 
     return jsonify({'result': 'success'})
 
@@ -111,8 +126,14 @@ def sign_up():
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
+    # exists = bool(db.users.find_one({"username": username_receive}))
+    sql = "SELECT * FROM users where username = '%s';"
+    cursor.execute(sql%(username_receive))
+    result = bool(cursor.fetchone())
+    
+
+    
+    return jsonify({'result': 'success', 'exists': result})
 
 # Profile 수정 Server
 @app.route('/update_profile', methods=['POST'])
@@ -136,99 +157,103 @@ def save_img():
             new_doc["profile_pic"] = filename
             new_doc["profile_pic_real"] = file_path
         db.users.update_one({'username': payload['id']}, {'$set':new_doc})
+
+
         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
 
-@app.route('/posting', methods=['POST'])
-def posting():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+# @app.route('/posting', methods=['POST'])
+# def posting():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-        # Posting API
-        user_info = db.users.find_one({"username": payload["id"]})
-        comment_receive = request.form["comment_give"]
-        date_receive = request.form["date_give"]
-
-        doc = {
-            "username": user_info["username"],
-            "profile_name": user_info["profile_name"],
-            "profile_pic_real": user_info["profile_pic_real"],
-            "comment": comment_receive,
-            "date": date_receive
-        }
-
-        db.posts.insert_one(doc)
-
-        return jsonify({"result": "success", 'msg': '포스팅 성공'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+#         # Posting API
+#         user_info = db.users.find_one({"username": payload["id"]})
 
 
-@app.route("/get_posts", methods=['GET'])
-def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         comment_receive = request.form["comment_give"]
+#         date_receive = request.form["date_give"]
 
-        # 서버 if문 & 문자열로 변환하기
-        username_receive = request.args.get("username_give")
-        if username_receive == "":
-            posts = list(db.posts.find({}).sort("date", -1).limit(20))
-        else:
-            posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
+#         doc = {
+#             "username": user_info["username"],
+#             "profile_name": user_info["profile_name"],
+#             "profile_pic_real": user_info["profile_pic_real"],
+#             "comment": comment_receive,
+#             "date": date_receive
+#         }
 
-        for post in posts:
-            post["_id"] = str(post["_id"])
+#         db.posts.insert_one(doc)
 
-            # post heart count(갯수 세기)
-            post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "heart"})
-            post["heart_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
-
-            # post star count
-            post["count_star"] = db.likes.count_documents({"post_id": post["_id"], "type": "star"})
-            post["star_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "star", "username": payload['id']}))
-
-            # post like count
-            post["count_like"] = db.likes.count_documents({"post_id": post["_id"], "type": "like"})
-            post["like_by_me"] = bool(
-                db.likes.find_one({"post_id": post["_id"], "type": "like", "username": payload['id']}))
-
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts":posts})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+#         return jsonify({"result": "success", 'msg': '포스팅 성공'})
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+#         return redirect(url_for("home"))
 
 
-@app.route('/update_like', methods=['POST'])
-def update_like():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+# @app.route("/get_posts", methods=['GET'])
+# def get_posts():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-        # Like Update API
-        user_info = db.users.find_one({"username": payload["id"]})
-        post_id_receive = request.form["post_id_give"]
-        type_receive = request.form["type_give"]
-        action_receive = request.form["action_give"]
-        doc = {
-            "post_id": post_id_receive,
-            "username": user_info["username"],
-            "type": type_receive
-        }
-        if action_receive == "like":
-            db.likes.insert_one(doc)
-        else:
-            db.likes.delete_one(doc)
-        count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
-        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+#         # 서버 if문 & 문자열로 변환하기
+#         username_receive = request.args.get("username_give")
+#         if username_receive == "":
+#             posts = list(db.posts.find({}).sort("date", -1).limit(20))
+#         else:
+#             posts = list(db.posts.find({"username": username_receive}).sort("date", -1).limit(20))
 
-        return jsonify({"result": "success", 'msg': 'updated'})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+#         for post in posts:
+#             post["_id"] = str(post["_id"])
+
+#             # post heart count(갯수 세기)
+#             post["count_heart"] = db.likes.count_documents({"post_id": post["_id"], "type": "heart"})
+#             post["heart_by_me"] = bool(
+#                 db.likes.find_one({"post_id": post["_id"], "type": "heart", "username": payload['id']}))
+
+#             # post star count
+#             post["count_star"] = db.likes.count_documents({"post_id": post["_id"], "type": "star"})
+#             post["star_by_me"] = bool(
+#                 db.likes.find_one({"post_id": post["_id"], "type": "star", "username": payload['id']}))
+
+#             # post like count
+#             post["count_like"] = db.likes.count_documents({"post_id": post["_id"], "type": "like"})
+#             post["like_by_me"] = bool(
+#                 db.likes.find_one({"post_id": post["_id"], "type": "like", "username": payload['id']}))
+
+#         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "posts":posts})
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+#         return redirect(url_for("home"))
+
+
+# @app.route('/update_like', methods=['POST'])
+# def update_like():
+#     token_receive = request.cookies.get('mytoken')
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+#         # Like Update API
+#         user_info = db.users.find_one({"username": payload["id"]})
+#         post_id_receive = request.form["post_id_give"]
+#         type_receive = request.form["type_give"]
+#         action_receive = request.form["action_give"]
+#         doc = {
+#             "post_id": post_id_receive,
+#             "username": user_info["username"],
+#             "type": type_receive
+#         }
+#         if action_receive == "like":
+#             db.likes.insert_one(doc)
+#         else:
+#             db.likes.delete_one(doc)
+#         count = db.likes.count_documents({"post_id": post_id_receive, "type": type_receive})
+#         return jsonify({"result": "success", 'msg': 'updated', "count": count})
+
+#         return jsonify({"result": "success", 'msg': 'updated'})
+#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+#         return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
